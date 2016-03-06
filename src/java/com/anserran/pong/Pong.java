ab7bef67249108dcb5aa3ba835473885b98fda92
anserran <angelserranolaguna@gmail.com>
2016-03-06 20:06:46 +0100

    Start

---
+++ src/java/com/anserran/pong/Pong.java
@@ -0,0 +1,10 @@
+package com.anserran.pong;
+
+public class Pong extends Game {
+	// Aquí deben ir las variables del juego
+
+	@Override
+	public void draw(float delta) {
+		// Aquí debe ir tu código de dibujado
+	}
+}
